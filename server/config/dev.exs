66f159611c78d2616e3e4f4a1655f2272fbfb886
Eric Teubert <eric@ericteubert.de>
2024-05-19 21:25:12 +0200

    wip

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -1,3 +1,7 @@
 import Config
 
+config :publisher, PublisherWeb.Endpoint,
+  ip: {0, 0, 0, 0, 0, 0, 0, 0},
+  port: "4005"
+
 config :logger, :console, format: "[$level] $message\n"
--- server/lib/publisher_web/controllers/api.ex
+++ server/lib/publisher_web/controllers/api.ex
@@ -35,4 +35,66 @@ defmodule PublisherWeb.Controllers.API do
         send_resp(conn, 400, "Error:  #{reason}")
     end
   end
+
+  def import_episode(conn, params) do
+    headers = conn.req_headers
+
+    user = get_header_value(headers, "wordpress-user")
+    password = get_header_value(headers, "wordpress-password")
+    site = get_header_value(headers, "wordpress-site")
+
+    # post_payload = %{
+    #   title: "LOV001 Lorem Ipsum"
+    # }
+
+    req =
+      Req.new(
+        base_url: site <> "/wp-json/",
+        headers: [{"Content-Type", "application/json"}],
+        auth: {:basic, user <> ":" <> password},
+        connect_options: [transport_opts: [verify: :verify_none]]
+      )
+
+    # CREATE EPISODE
+    {:ok, create_episode_response} = Req.post(req, url: "podlove/v2/episodes")
+
+    episode_id = create_episode_response.body["id"]
+    IO.inspect("episode created. id: #{episode_id}")
+
+    # Fetch just created episode
+    {:ok, fetched_episode_response} = Req.get(req, url: "podlove/v2/episodes/#{episode_id}")
+
+    episode = fetched_episode_response.body
+    post_id = episode["post_id"]
+
+    # Status: we have a post+episode and their ids. Both are kind of empty.
+
+    # Now we write the episode meta.
+    {:ok, updated_episode} =
+      Req.post(req,
+        url: "podlove/v2/episodes/#{episode_id}",
+        json: %{
+          title: "LOV001 Lorem Ipsum",
+          subtitle: "Talking about endangered species.",
+          summary: "The most exciting episode on the planet. And the universe.",
+          number: "1",
+          explicit: "false",
+          slug: "lov001-lorem-ipsum",
+          duration: "00:00:42.123",
+          type: "full"
+          # episode_poster
+        }
+      )
+
+    IO.inspect(updated_episode)
+
+    json(conn, %{status: "success"})
+  end
+
+  defp get_header_value(headers, header_item) do
+    case Enum.find(headers, fn {name, _} -> name == header_item end) do
+      nil -> nil
+      {_, value} -> value
+    end
+  end
 end
