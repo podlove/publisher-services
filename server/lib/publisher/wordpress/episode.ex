7bd8f942130ec49a8525a421807fd7c1a044a061
Eric Teubert <eric@ericteubert.de>
2024-05-24 12:19:05 +0200

    refactor: extract steps into private functions

--- server/lib/publisher/wordpress/episode.ex
+++ server/lib/publisher/wordpress/episode.ex
@@ -4,70 +4,13 @@ defmodule Publisher.WordPress.Episode do
   def save(conn, params) do
     req = API.new(conn.req_headers)
 
-    episode_id = find_or_create_episode(req, params["guid"])
-
-    # Fetch episode, so we get the post_id
-    # TODO: can be skipped if we return the post_id in the CREATE response
-    {:ok, %Req.Response{body: episode}} = Req.get(req, url: "podlove/v2/episodes/#{episode_id}")
-
-    post_id = episode["post_id"]
-
-    # Now we write the episode meta.
-    {:ok, _updated_episode} =
-      Req.post(req,
-        url: "podlove/v2/episodes/#{episode_id}",
-        json: %{
-          guid: params["guid"],
-          title: params["title"],
-          subtitle: params["subtitle"],
-          summary: params["summary"],
-          number: params["number"],
-          explicit: params["explicit"],
-          slug: params["slug"],
-          duration: params["duration"],
-          type: params["type"] || "full"
-          # episode_poster
-        }
-      )
-
-    enclosure_url = params["enclosure"]
-
-    ext = extension_from_url(enclosure_url)
-
-    filename = [params["slug"], ext] |> Enum.join(".")
-
-    {:ok, resp} = Req.get(enclosure_url)
-
-    {:ok, upload} =
-      Req.post(req,
-        url: "wp/v2/media",
-        params: [post: post_id],
-        headers: [
-          {"Content-Type", content_type(resp)},
-          {"Content-Disposition", "attachment; filename=\"" <> filename <> "\""}
-        ],
-        body: resp.body
-      )
-
-    if upload.body["generated_slug"] != params["slug"] do
-      # TODO: use the generated_slug for the episode, in case there are
-      # duplicates. Otherwise the url will point to a wrong audio file.
+    with episode_id <- find_or_create_episode(req, params["guid"]),
+         post_id <- fetch_post_id(req, episode_id),
+         {:ok, _} <- write_episode_meta(req, episode_id, params),
+         :ok <- upload_media(req, episode_id, post_id, params),
+         :ok <- verify_media(req, episode_id) do
+      :ok
     end
-
-    {:ok, assets} = Req.get(req, url: "podlove/v2/episodes/#{episode_id}/media")
-    asset_ids = Enum.map(assets.body["results"], & &1["asset_id"])
-
-    verify_results =
-      Enum.map(asset_ids, fn asset_id ->
-        {:ok, _result} =
-          Req.post(req, url: "podlove/v2/episodes/#{episode_id}/media/#{asset_id}/verify")
-
-        # TODO: What should we verify here? Just that result.status == 200? Because
-        # there might be more than just one asset, so we don't know which one MUST have
-        # a size. Unless we figure that out beforehand.
-      end)
-
-    :ok
   end
 
   def content_type(resp) do
@@ -109,5 +52,71 @@ defmodule Publisher.WordPress.Episode do
       episode_id ->
         episode_id
     end
+  end
+
+  # TODO: can be skipped if we return the post_id in the CREATE response
+  defp fetch_post_id(req, episode_id) do
+    {:ok, %Req.Response{body: episode}} = Req.get(req, url: "podlove/v2/episodes/#{episode_id}")
+    episode["post_id"]
+  end
+
+  defp write_episode_meta(req, episode_id, params) do
+    Req.post(req,
+      url: "podlove/v2/episodes/#{episode_id}",
+      json: %{
+        guid: params["guid"],
+        title: params["title"],
+        subtitle: params["subtitle"],
+        summary: params["summary"],
+        number: params["number"],
+        explicit: params["explicit"],
+        slug: params["slug"],
+        duration: params["duration"],
+        type: params["type"] || "full"
+        # episode_poster
+      }
+    )
+  end
+
+  defp upload_media(req, _episode_id, post_id, params) do
+    enclosure_url = params["enclosure"]
+    ext = extension_from_url(enclosure_url)
+    filename = [params["slug"], ext] |> Enum.join(".")
+
+    {:ok, resp} = Req.get(enclosure_url)
+
+    {:ok, upload} =
+      Req.post(req,
+        url: "wp/v2/media",
+        params: [post: post_id],
+        headers: [
+          {"Content-Type", content_type(resp)},
+          {"Content-Disposition", "attachment; filename=\"" <> filename <> "\""}
+        ],
+        body: resp.body
+      )
+
+    if upload.body["generated_slug"] != params["slug"] do
+      # TODO: use the generated_slug for the episode, in case there are
+      # duplicates. Otherwise the url will point to a wrong audio file.
+    end
+
+    :ok
+  end
+
+  defp verify_media(req, episode_id) do
+    {:ok, assets} = Req.get(req, url: "podlove/v2/episodes/#{episode_id}/media")
+    asset_ids = Enum.map(assets.body["results"], & &1["asset_id"])
+
+    Enum.map(asset_ids, fn asset_id ->
+      {:ok, _result} =
+        Req.post(req, url: "podlove/v2/episodes/#{episode_id}/media/#{asset_id}/verify")
+
+      # TODO: What should we verify here? Just that result.status == 200? Because
+      # there might be more than just one asset, so we don't know which one MUST have
+      # a size. Unless we figure that out beforehand.
+    end)
+
+    :ok
   end
 end
