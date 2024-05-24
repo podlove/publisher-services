158e08337ddf6b8c252b69836dd2056f2dacdf36
Eric Teubert <eric@ericteubert.de>
2024-05-24 12:02:04 +0200

    chore: allow port override in dev

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -1,7 +1,3 @@
 import Config
 
-config :publisher, PublisherWeb.Endpoint,
-  ip: {0, 0, 0, 0, 0, 0, 0, 0},
-  port: "4005"
-
 config :logger, :console, format: "[$level] $message\n"
--- server/config/runtime.exs
+++ server/config/runtime.exs
@@ -1,9 +1,7 @@
 import Config
 
-if config_env() == :prod do
-  port = String.to_integer(System.get_env("PORT") || "4000")
+port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :publisher, PublisherWeb.Endpoint,
-    ip: {0, 0, 0, 0, 0, 0, 0, 0},
-    port: port
-end
+config :publisher, PublisherWeb.Endpoint,
+  ip: {0, 0, 0, 0, 0, 0, 0, 0},
+  port: port
