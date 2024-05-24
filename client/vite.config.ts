8a4dfe079f8213c594c60b1bc68d67f578df93d4
Eric Teubert <eric@ericteubert.de>
2024-05-24 12:04:29 +0200

    chore: undo temporary dev change

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4005',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
         ws: true
