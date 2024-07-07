1f6533b22650c05617d5111a9e542cd61798578c
Alexander Heimbuch <github@heimbu.ch>
2024-07-07 10:56:21 +0200

    ci: stabilize login

--- e2e/cypress/support/commands.ts
+++ e2e/cypress/support/commands.ts
@@ -2,8 +2,11 @@ import { loginButton, passwordInput, userInput } from "../selectors/login";
 
 Cypress.Commands.add('login', (user, password) => {
   cy.visit('/wp-admin');
-  userInput().should('exist').type(user);
-  passwordInput().should('exist').type(password);
+  cy.wait(250);
+  userInput().should('exist').type(user, { delay: 100 });
+  cy.wait(250);
+  passwordInput().should('exist').type(password, { delay: 100 });
+  cy.wait(250);
   loginButton().should('exist').click();
 });
 
