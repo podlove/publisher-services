b69d3958832999b5fe7e38fbbc7b71b90f362066
Alexander Heimbuch <github@heimbu.ch>
2024-07-07 17:35:03 +0200

    chore: improve steps scrolling behavior

--- client/src/pages/onboarding/Onboarding.vue
+++ client/src/pages/onboarding/Onboarding.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="h-full flex flex-col" :data-test="`step-${state.current.name}`">
     <Steps :steps="state.steps" v-if="state.current && state.current.visible"></Steps>
-    <div class="p-4 sm:px-6 xl:pl-6">
+    <div class="pt-4 sm:px-6 xl:pl-6 overflow-y-auto" :style="contentStyle">
       <component :is="stepComponents[state.current.name]" />
     </div>
-    <div class="flex justify-between w-full px-4 sm:px-6 xl:pl-6">
+    <div class="flex justify-between w-full px-4 sm:px-6 xl:pl-6 py-2">
       <PodloveButton v-if="state.previous" variant="secondary" @click="prevStep()">{{
         t('onboarding.navigation.prev')
       }}</PodloveButton>
@@ -17,6 +17,7 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, computed } from 'vue';
 import { mapState, injectStore } from 'redux-vuex';
 import { useI18n } from 'vue-i18n';
 import { Action } from 'redux';
@@ -46,11 +47,14 @@ const state = mapState({
   upcomingEnabled: selectors.onboarding.upcomingEnabled,
 });
 
+const contentStyle = computed(() => ({
+  height: state.current && state.current.visible ? `calc(100vh - 75px - 35px)` : `calc(100vh - 35px)`
+}));
+
 const stepComponents = {
   select: SetupType,
   'start-new-podcast': StartNewPodcast,
   'start-new-next-steps': StartNewNextSteps,
-
   'import-feed': ImportFeed,
   'import-podcast': ImportPodcast,
   'import-episodes': ImportEpisodes,
