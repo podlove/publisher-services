1bd1387076a55d7a5cac7a721f35c07e46247273
Dirk Schumann <dirk@freunde-aus-berlin.net>
2024-09-09 15:55:21 +0200

    fix(server) - media upload integrate PR feedback

--- server/lib/publisher/wordpress/api.ex
+++ server/lib/publisher/wordpress/api.ex
@@ -4,35 +4,31 @@ defmodule Publisher.WordPress.API do
 
   ## Example
 
-    req = API.new(headers, http1)
+    req = API.new(headers)
     {:ok, response} = Req.get(req, url: "podlove/v2/episodes")
 
   """
-  def new(headers, http1) do
+  def new(headers, opts \\ []) do
     user = get_header_value(headers, "wordpress-user")
     password = get_header_value(headers, "wordpress-password")
     site = get_header_value(headers, "wordpress-site")
 
-    if (http1) do
-      Req.new(
-        base_url: site <> "/wp-json/",
-        headers: [{"Content-Type", "application/json"}],
-        auth: {:basic, user <> ":" <> password},
-        connect_options: [
-          transport_opts: [verify: :verify_none]
-        ]
-      )
-    else
-      Req.new(
-        base_url: site <> "/wp-json/",
-        headers: [{"Content-Type", "application/json"}],
-        auth: {:basic, user <> ":" <> password},
-        connect_options: [
-          protocols: [:http1],
-          transport_opts: [verify: :verify_none]
-        ]
-      )
+    default_connect_options = [transport_opts: [verify: :verify_none]]
+    http1 = Keyword.get(opts, :http1, false)
+
+    connect_options =
+      if http1 do
+        Keyword.put(default_connect_options, :protocols, [:http1])
+      else
+        default_connect_options
       end
+
+    Req.new(
+      base_url: site <> "/wp-json/",
+      headers: [{"Content-Type", "application/json"}],
+      auth: {:basic, user <> ":" <> password},
+      connect_options: connect_options
+    )
   end
 
   defp get_header_value(headers, header_item) do
